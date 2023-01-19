eedbd59b4273cf8f760ca144ad9bd807df4fb86f
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-19 08:53:48 -0400

    feat: add fetchData

--- src/api/cosumoAPI_cat/main.js
+++ src/api/cosumoAPI_cat/main.js
@@ -1,3 +1,21 @@
-const API = "https://api.thecatapi.com/v1/images";
+const API = "https://api.thecatapi.com/v1/images/search";
 
-fetch(`${API}/search`);
+async function fetchData(urlAPI) {
+  const response = await fetch(urlAPI);
+  const data = await response.json();
+  return data;
+}
+
+const consume_API = async (urlAPI) => {
+  try {
+    const img = await fetchData(`${urlAPI}`);
+    const imgHTML = document.querySelector("img");
+    imgHTML.src = img[0].url;
+    // mostramos en la consola
+    console.log(img[0].url);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+consume_API(API);
